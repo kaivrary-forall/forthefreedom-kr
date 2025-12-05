462b055e6fc9a3c9e7faa232a997134818ca3d42
kaivrary-forall <forall@kaivrary.com>
2025-12-06 04:37:18 +0900

    Add files via upload

--- server/utils/email.js
+++ server/utils/email.js
@@ -5,9 +5,11 @@
 
 const nodemailer = require('nodemailer');
 
-// SMTP 설정
+// SMTP 설정 (Gmail - SSL 포트 465)
 const transporter = nodemailer.createTransport({
-  service: 'gmail',
+  host: 'smtp.gmail.com',
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS
