68218d1c3e0160d7cbb5252540434a5cfc15bd67
kaivrary-forall <forall@kaivrary.com>
2026-01-11 20:31:52 +0900

    Update page.tsx

--- client/app/mypage/page.tsx
+++ client/app/mypage/page.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from 'next'
-import MyPageContent from '@/components/mypage/MyPageContent'
+import MyPageContent from '@/components/auth/MyPageContent'
 
 export const metadata: Metadata = {
   title: '마이페이지 | 자유와혁신',
