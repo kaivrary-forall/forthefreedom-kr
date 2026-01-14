a43d7a509ac4de28d75c2a55b418f1686fa85100
kaivrary-forall <forall@kaivrary.com>
2026-01-14 16:20:50 +0900

    Add files via upload

--- client/components/about/AboutTabs.tsx
+++ client/components/about/AboutTabs.tsx
@@ -21,7 +21,7 @@ export default function AboutTabs() {
     <div>
       {/* 탭 네비게이션 */}
       <div 
-        className="sticky z-30 bg-white border-b border-gray-200"
+        className="sticky z-30 bg-white border-b border-gray-200 transition-[top] duration-300 ease-in-out"
         style={{ top: 'calc(64px + var(--top-notice-h, 0px))' }}
       >
         <div className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8">
