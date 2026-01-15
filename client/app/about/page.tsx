fda607973f3357219924319fd1cdfcbd5c59106a
kaivrary-forall <forall@kaivrary.com>
2026-01-15 09:46:33 +0900

    Add files via upload

--- client/app/about/page.tsx
+++ client/app/about/page.tsx
@@ -1,17 +1,5 @@
-import type { Metadata } from 'next'
-import AboutTabs from '@/components/about/AboutTabs'
-
-export const metadata: Metadata = {
-  title: '황교안 대표 소개 | 자유와혁신 당소개',
-  description: '황교안 자유와혁신 대표 소개 및 당 소개. 창당 배경, 정체성, 미래 비전을 확인하세요.',
-  openGraph: {
-    title: '황교안 대표 소개 | 자유와혁신 당소개',
-    description: '황교안 자유와혁신 대표 소개 및 당 소개. 창당 배경과 정체성, 미래 비전을 확인하세요',
-    url: 'https://forthefreedom.kr/about',
-    images: ['/images/profile-pic.jpg'],
-  },
-}
+import { redirect } from 'next/navigation'
 
 export default function AboutPage() {
-  return <AboutTabs />
+  redirect('/about/message')
 }
